570e719c7e0491b0d827f07d153f54540c363386
Kieran Watters <2659434w@student.gla.ac.uk>
2024-01-16 21:26:57 +0000

    added about view

--- rango/views.py
+++ rango/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return HttpResponse("Rango says hey there partner!")
+
+
+def about(request):
+    return HttpResponse("Rango says here is the about page. ")
