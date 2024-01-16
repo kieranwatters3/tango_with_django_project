e4e56583be163a182c500c3bf602ea78b53f922e
Kieran Watters <2659434w@student.gla.ac.uk>
2024-01-16 21:30:45 +0000

    added hyperlinks

--- rango/views.py
+++ rango/views.py
@@ -4,8 +4,8 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
+    return HttpResponse("<a href='/rango/about/'>About</a> Rango says hey there partner!")
 
 
 def about(request):
-    return HttpResponse("Rango says here is the about page. ")
+    return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a> ")
