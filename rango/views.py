dd2475206da0ae6829fafc2fb3bbf2ad11de2e38
Kieran Watters <2659434w@student.gla.ac.uk>
2024-01-19 19:58:30 +0000

    ch4 complete few tests not working

--- rango/views.py
+++ rango/views.py
@@ -5,9 +5,9 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 
-    context_dict = {"boldmessage": "Crunchy, creamy, cooky, candy, cupcake!"}
+    context_dict = {"boldmessage": "Crunchy, creamy, cookie, candy, cupcake!"}
     return render(request, "rango/index.html", context=context_dict)
 
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.<a href='/rango/'>Index</a> ")
+    return render(request, "rango/about.html")
