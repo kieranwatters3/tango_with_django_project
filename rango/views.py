033b66700aa4ffab7e71294dcabb5cb67e5e1ee0
Kieran Watters <2659434w@student.gla.ac.uk>
2024-02-09 17:48:43 +0000

    ch10 done

--- rango/views.py
+++ rango/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth import authenticate, login
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
+from datetime import datetime
 
 
 # Create your views here.
@@ -21,14 +22,19 @@ def index(request):
     context_dict["boldmessage"] = "Crunchy, creamy, cookie, candy, cupcake!"
     context_dict["categories"] = category_list
     context_dict["pages"] = page_list
-
-    return render(request, "rango/index.html", context=context_dict)
+    visitor_cookie_handler(request)
+    #context_dict["visits"] = request.session['visits']
+    response = render(request, 'rango/index.html', context=context_dict)
+    return response
 
 
 def about(request):
     print(request.method)
     print(request.user)
-    return render(request,'rango/about.html',{})
+    visitor_cookie_handler(request)
+    context_dict = {}
+    context_dict["visits"] = request.session['visits']
+    return render(request, 'rango/about.html', context=context_dict)
 
 
 def show_category(request, category_name_slug):
@@ -146,3 +152,22 @@ def restricted(request):
 def user_logout(request):
     logout(request)
     return redirect(reverse('rango:index'))
+
+
+def visitor_cookie_handler(request):
+    visits = int(request.COOKIES.get('visits', '1'))
+    last_visit_cookie = request.COOKIES.get('last_visit', str(datetime.now()))
+    last_visit_time = datetime.strptime(last_visit_cookie[:-7], '%Y-%m-%d %H:%M:%S')
+    if (datetime.now() - last_visit_time).days > 0:
+        visits = visits + 1
+        request.session['last_visit'] = str(datetime.now())
+    else:
+        request.session['last_visit'] = last_visit_cookie
+    request.session['visits'] = visits
+
+
+def get_server_side_cookie(request, cookie,default_val=None):
+    val = request.session.get(cookie)
+    if not val:
+        val = default_val
+    return val
